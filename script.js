20eb19dec1939f5b419ac618bfe9c6cd39de9ea8
bobyangg <bobyang2004@gmail.com>
2021-08-13 14:18:35 -0400

    cool

---
+++ script.js
@@ -0,0 +1,4 @@
+<button id="myButton" class="buttons" >Home</button>
+document.getElementById("myButton").onclick = function () {
+    location.href = "C:\Users\bober\Downloads\set hacks\digital_healthcare.html";
+};
